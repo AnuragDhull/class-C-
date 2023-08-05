a84eb52e22a42247e16cad696fde43ede1cbf913
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2023-08-05 13:56:32 +0530

    Add files via upload

---
+++ class2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1=20;
+    double num2=30.0909;
+    char ch='a';
+    cout<<"first number"<<(float) num1<<endl;
+    cout<<"second number"<<num2<<endl;
+    cout<<"character "<<ch<<endl;
+    return 0;
+}
+
+
---
+++ class5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // Naming of Variabe :-
+    // no space not allowed
+    // white space not allowed
+    // reserved word are not allowed
+    // meaningful names 
+    
+}

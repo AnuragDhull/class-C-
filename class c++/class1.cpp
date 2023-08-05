f34b016c80c745046f37be447e3fc7fd979fed21
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2023-08-05 13:54:37 +0530

    Add files via upload

---
+++ class c++/class1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+// Structure of C++
+
+// 1.) DOcumentation Section(comments)                       
+
+
+// 2.) Header Files                       
+
+// 3.) Global values                       
+
+// 4.) Class Declaration and defination
+
+// 5.) Function declartion and defination
+
+// 6.) main()
+
+
+// difference between \n and endl 
+// endl is manipulator , no bytes are required 
+// \n is character , 1 byte is required
+}
---
+++ class c++/class3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1=78;
+    double num2=79;
+    char myLetter='a';
+    string myText="anurag";
+    bool mybool=true;
+    cout<<num1<<endl<<num2<<endl<<myLetter<<endl<<myText<<endl<<mybool<<endl;
+    cout<<"my first num1 :-"<<num1<<endl;
+    return 0;
+
+
+}
